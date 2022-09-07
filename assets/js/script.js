e3eed613084ef9988dd172f8798d3d8db0894c12
Nathy Rodrigues <nathalia.r.s@outlook.com>
2022-09-07 16:09:12 -0300

    Finalização projeto

---
+++ assets/js/script.js
@@ -0,0 +1,106 @@
+const section = document.querySelector("section")
+
+function cardVagas(vaga) {
+    const containerCard = document.createElement("div")
+    containerCard.classList.add("cards-vagas", "bg-color-grey-6")
+
+    const tituloVaga = document.createElement("h2")
+    tituloVaga.classList.add("title-4", "color-grey'1")
+    tituloVaga.innerText = vaga.nome
+
+    const empresa = document.createElement("span")
+    empresa.classList.add("text-3", "color-grey-2", "span-card")
+    empresa.innerText = vaga.empresa
+
+    const localEmpresa = document.createElement("span")
+    localEmpresa.classList.add("text-3", "color-grey-2", "span-card")
+    localEmpresa.innerText = vaga.localEmpresa
+
+    const descricao = document.createElement("p")
+    descricao.classList.add("text-2", "color-grey-2")
+    descricao.innerText = vaga.descricao
+
+    const boxCandidatar = document.createElement("div")
+    boxCandidatar.classList.add("box-candidatar")
+
+    const tipo = document.createElement("span")
+    tipo.classList.add("text-3", "color-grey-2")
+    tipo.innerText = vaga.tipo
+
+    const buttonCandidatar = document.createElement("button")
+    buttonCandidatar.classList.add("little-btn", "bg-color-brand-1", "color-grey-6")
+    buttonCandidatar.id = vaga.id
+    buttonCandidatar.innerText = "Candidatar"
+
+    boxCandidatar.append(tipo, buttonCandidatar)
+    containerCard.append(tituloVaga, empresa, localEmpresa, descricao, boxCandidatar)
+
+    return containerCard
+}
+
+function listarVagas() {
+    for (let i = 0; i < vagas.length; i++) {
+        let card = cardVagas(vagas[i])
+        section.appendChild(card)
+    }
+}
+
+listarVagas()
+
+let vagasSelecionadas = []
+const containerVagasSelecionadas = document.querySelector("#container-vagas-selecionadas")
+
+function cardVagaSelecionada(vaga) {
+    const card = document.createElement("li")
+    card.classList.add("vaga-selecionada-card")
+
+    card.innerHTML = `
+        <div class = "flex-row">
+            <h3>${vaga.nome}</h3>
+
+            <button class = "trash-btn bg-color-grey-5" id=${vaga.id}>
+                <img src="/assets/img/trash.svg" alt="ícone de lixeira" />
+            </button>
+    </div>
+
+        <div class="flex-span">
+            <span class = "color-grey-2 text-3">${vaga.empresa}</span>
+            <span class = "color-grey-2 text-3">${vaga.localEmpresa}</span>
+        </div>
+    `
+    return card
+}
+
+section.addEventListener("click", function (event) {
+    const botao = event.target
+
+    if (botao.tagName == "BUTTON") {
+        const idVaga = botao.id
+        const vagaFiltrada = vagas.find((vaga) => vaga.id == idVaga)
+
+        vagasSelecionadas.push(vagaFiltrada)
+        listarVagasSelecionadas()
+    }
+
+})
+
+function listarVagasSelecionadas() {
+    containerVagasSelecionadas.innerHTML = ""
+    for (let i = 0; i < vagasSelecionadas.length; i++) {
+        const card = cardVagaSelecionada(vagasSelecionadas[i])
+        containerVagasSelecionadas.appendChild(card)
+    }
+}
+
+containerVagasSelecionadas.addEventListener("click", removerVaga)
+function removerVaga(event) {
+    const botaoLixeira = event.target
+    if (botaoLixeira.tagName == "BUTTON" || botaoLixeira.tagName == "IMG") {
+        botaoLixeira.closest("li").remove()
+
+
+        const vagaDuplicada = vagas.find((vaga) => vaga.id == idVaga)
+        vagasSelecionadas.indexOf(vagaDuplicada, [0])
+        vagasSelecionadas.splice(vagaDuplicada, 1)
+    }
+}
---
+++ assets/js/vagas.js
@@ -0,0 +1,26 @@
+const vagas = [
+    {
+        id: 1,
+        nome: "Pessoa desenvolvedora front-end - React JS",
+        empresa: "Kenzie Academy",
+        localEmpresa: "Curitiba",
+        descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        tipo: "Home Office",
+    },
+    {
+        id: 2,
+        nome: "Pessoa desenvolvedora back-end - Node JS",
+        empresa: "Brazilians in Tech",
+        localEmpresa: "Rio de Janeiro",
+        descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        tipo: "Presencial",
+    },
+    {
+        id: 3,
+        nome: "Pessoa desenvolvedora Full Stack",
+        empresa: "Brazilians in Tech",
+        localEmpresa: "São Paulo",
+        descricao: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        tipo: "Híbrido",
+    }
+]
